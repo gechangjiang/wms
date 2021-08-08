04b474def8031641703790ba7a4a736b66b6a106
gechangjiang <812829269@qq.com>
2021-08-08 14:17:53 +0800

    创建

---
+++ code/tdt-base-support/tdt-base/src/main/java/com/tdt/base/consts/ConfigConstant.java
@@ -0,0 +1,16 @@
+package com.tdt.base.consts;
+
+/**
+ * 配置的常量
+ *
+ * @author www.qiqucode.com
+ * @date 2019-06-24-12:51
+ */
+public interface ConfigConstant {
+
+    /**
+     * 系统常量的前缀标识
+     */
+    String SYSTEM_CONSTANT_PREFIX = "TDT_";
+
+}
